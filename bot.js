a9aa654c71131777c51331fb1b31633df519a819
KNIGHT10100 <77894241+KNIGHT10100@users.noreply.github.com>
2021-01-23 23:02:45 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,10 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "381443488392413184" && message.channel.id === "381397780385628181"){
-        if(msg.startsWith('>', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "381397780385628181"){
-                message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
+    if(message.author.id != "802583574045130782" && message.channel.id === "799167062739386368"){
+        if(msg.startsWith('.', 0)){
+            if(msg === prefix + "bang" && message.channel.id === "799167062739386368"){
+                message.channel.send('hello and namaste') // Sends a message to the channel, with the content
             }
             else{
                 message.channel.send("Hey, I only understand >bang right now. More will be added when my creator is free. :) ")
